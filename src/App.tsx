66a7647d251e78f065e3db0a67f113afa5e649db
DOF1109 <friasdaniel.1109@gmail.com>
2025-04-06 12:58:26 -0300

    Generic Appbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CssBaseline } from "@mui/material";
 import Dashboard from "./pages/dashboard/Dashboard";
+import Appbar from "./components/Appbar";
 
 const App = () => {
   return (
     <>
       <CssBaseline />
+      <Appbar />
       <Dashboard />
     </>
   );

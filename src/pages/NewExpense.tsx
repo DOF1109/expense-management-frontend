9599cd0dab7d3b76282808df32bdfaae35b5bae0
DOF1109 <friasdaniel.1109@gmail.com>
2025-04-19 17:17:09 -0300

    Aadd Formik, Yup, Dayjs, and MUI Date Pickers; enhance NewExpense component with form handling and validation

--- src/pages/NewExpense.tsx
+++ src/pages/NewExpense.tsx
@@ -1,7 +1,92 @@
-const NewExpense = () => {
-  return (
-    <div>NewExpense</div>
-  )
-}
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+import { useFormik } from "formik";
+import { Expense } from "../model/Expense";
+import * as Yup from "yup";
 
-export default NewExpense
+const expenseValidationSchema = Yup.object({
+  name: Yup.string().required("El nombre es requerido"),
+});
+
+const NewExpense = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      amount: 0,
+      date: new Date(),
+      category: "",
+      note: "",
+    },
+    onSubmit: (values: Expense) => {
+      console.log(values);
+    },
+    validationSchema: expenseValidationSchema,
+  });
+
+  return (
+    <Box component="section">
+      <Grid
+        container
+        component="form"
+        onSubmit={formik.handleSubmit}
+        spacing={2}
+        sx={{ py: 3, justifyContent: "center" }}
+      >
+        <Grid size={{ xs: 12, sm: 9, md: 8, lg: 7 }}>
+          <TextField
+            name="name"
+            label="Nombre del gasto"
+            variant="outlined"
+            fullWidth
+            onChange={formik.handleChange}
+            error={formik.errors.name ? true : false}
+            helperText={formik.errors.name}
+          />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 9, md: 8, lg: 7 }}>
+          <TextField
+            name="amount"
+            label="Monto del gasto"
+            variant="outlined"
+            fullWidth
+            onChange={formik.handleChange}
+            type="number"
+          />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 9, md: 8, lg: 7 }}>
+          <DatePicker name="date" label="Fecha" />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 9, md: 8, lg: 7 }}>
+          <TextField
+            name="category"
+            label="Categoría del gasto"
+            variant="outlined"
+            fullWidth
+            onChange={formik.handleChange}
+          />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 9, md: 8, lg: 7 }}>
+          <TextField
+            name="note"
+            label="Nota del gasto"
+            variant="outlined"
+            fullWidth
+            onChange={formik.handleChange}
+            multiline
+            rows={5}
+          />
+        </Grid>
+        <Grid
+          size={{ xs: 12, sm: 9, md: 8, lg: 7 }}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <Button type="submit" variant="contained">
+            Guardar
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default NewExpense;
--- src/styles/themeConfig.ts
+++ src/styles/themeConfig.ts
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material/styles";
+// import type {} from '@mui/x-date-pickers/themeAugmentation';
 
 const theme = createTheme({
   palette: {
@@ -6,6 +7,20 @@ const theme = createTheme({
       main: "#2B2F33",
     },
   },
+  // components: {
+  //   MuiDatePicker: {
+  //     defaultProps: {
+  //       displayWeekNumber: true,
+  //     },
+  //   },
+  //   MuiDateRangeCalendar: {
+  //     styleOverrides: {
+  //       root: {
+  //         backgroundColor: '#f0f0f0',
+  //       },
+  //     },
+  //   },
+  // },
 });
 
 export default theme;
